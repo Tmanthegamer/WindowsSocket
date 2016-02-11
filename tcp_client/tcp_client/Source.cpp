adffede70919dbaffd12df12d4139f3be1c6fa9b
tmanthegamer <tmanthegamer@gmail.com>
2016-02-10 20:10:33 -0800

    Doing some testing.

--- tcp_client/tcp_client/Source.cpp
+++ tcp_client/tcp_client/Source.cpp
@@ -27,41 +27,46 @@
 #define BUFSIZE	255				//Buffer length
 #define TRUE	1
 
+#define BUFSIZE					255		// Buffer length
+
 int main(int argc, char **argv)
 {
-	int	n, ns, bytes_to_read;
-	int	client_len, port, err;
-	SOCKET sd, new_sd;
-	struct	sockaddr_in server, client;
-	char	*bp, buf[BUFSIZE];
+	int n, ns, bytes_to_read;
+	int port, err;
+	SOCKET sd;
+	struct hostent	*hp;
+	struct sockaddr_in server;
+	char  *host, *bp, rbuf[BUFSIZE], sbuf[BUFSIZE], **pptr;
 	WSADATA WSAData;
 	WORD wVersionRequested;
 
 	switch (argc)
 	{
-	case 1:
-		port = SERVER_TCP_PORT;	// Use the default port
-		break;
 	case 2:
-		port = atoi(argv[1]);	// Get user specified port
+		host = argv[1];	// Host name
+		port = SERVER_TCP_PORT;
+		break;
+	case 3:
+		host = argv[1];
+		port = atoi(argv[2]);	// User specified port
 		break;
 	default:
-		fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+		fprintf(stderr, "Usage: %s host [port]\n", argv[0]);
 		exit(1);
 	}
 
 	wVersionRequested = MAKEWORD(2, 2);
 	err = WSAStartup(wVersionRequested, &WSAData);
-	if (err != 0) //No useable DLL
+	if (err != 0) //No usable DLL
 	{
 		printf("DLL not found!\n");
 		exit(1);
 	}
 
-	// Create a stream socket
+	// Create the socket
 	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
-		perror("Can't create a socket");
+		perror("Cannot create socket");
 		exit(1);
 	}
 
@@ -69,42 +74,44 @@ int main(int argc, char **argv)
 	memset((char *)&server, 0, sizeof(struct sockaddr_in));
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port);
-	server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
-
-												// Bind an address to the socket
-	if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+	if ((hp = gethostbyname(host)) == NULL)
 	{
-		perror("Can't bind name to socket");
+		fprintf(stderr, "Unknown server address\n");
 		exit(1);
 	}
 
-	// Listen for connections
-	// queue up to 5 connect requests
-	listen(sd, 5);
+	// Copy the server address
+	memcpy((char *)&server.sin_addr, hp->h_addr, hp->h_length);
 
-	while (TRUE)
+	// Connecting to the server
+	if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1)
 	{
-		client_len = sizeof(client);
-		if ((new_sd = accept(sd, (struct sockaddr *)&client, &client_len)) == -1)
-		{
-			fprintf(stderr, "Can't accept client\n");
-			exit(1);
-		}
-
-		printf(" Remote Address:  %s\n", inet_ntoa(client.sin_addr));
-		bp = buf;
-		bytes_to_read = BUFSIZE;
-		while ((n = recv(new_sd, bp, bytes_to_read, 0)) < BUFSIZE)
-		{
-			bp += n;
-			bytes_to_read -= n;
-			if (n == 0)
-				break;
-		}
-
-		ns = send(new_sd, buf, BUFSIZE, 0);
-		closesocket(new_sd);
+		fprintf(stderr, "Can't connect to server\n");
+		perror("connect");
+		exit(1);
 	}
+	printf("Connected:    Server Name: %s\n", hp->h_name);
+	pptr = hp->h_addr_list;
+	printf("\t\tIP Address: %s\n", inet_ntoa(server.sin_addr));
+	printf("Transmiting:\n");
+	memset((char *)sbuf, 0, sizeof(sbuf));
+	fgets(sbuf, sizeof(sbuf), stdin); // get user's text
+
+				// Transmit data through the socket
+	ns = send(sd, sbuf, BUFSIZE, 0);
+	printf("Receive:\n");
+	bp = rbuf;
+	bytes_to_read = BUFSIZE;
+
+	// client makes repeated calls to recv until no more data is expected to arrive.
+	while ((n = recv(sd, bp, bytes_to_read, 0)) < BUFSIZE)
+	{
+		bp += n;
+		bytes_to_read -= n;
+		if (n == 0)
+			break;
+	}
+	printf("%s\n", rbuf);
 	closesocket(sd);
 	WSACleanup();
 	exit(0);
