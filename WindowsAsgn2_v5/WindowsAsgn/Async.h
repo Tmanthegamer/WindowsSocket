80f9a8a4a4d52c22b9117f637e788b7e8e84aed7
tmanthegamer <tmanthegamer@gmail.com>
2016-02-15 08:00:40 -0800

    Final product.

---
+++ WindowsAsgn2_v5/WindowsAsgn/Async.h
@@ -0,0 +1,26 @@
+#pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#pragma comment(lib, "Ws2_32.lib")
+
+#define PORT 5150
+#define DATA_BUFSIZE 65535
+
+#define ACK 0x06
+#define SOT 0x02
+#define EOT 0x04
+
+typedef struct _SOCKET_INFORMATION {
+	BOOL RecvPosted;
+	CHAR Buffer[DATA_BUFSIZE];
+	OVERLAPPED Overlapped;
+	WSABUF DataBuf;
+	SOCKET Socket;
+	DWORD BytesSEND;
+	DWORD BytesRECV;
+	_SOCKET_INFORMATION *Next;
+} SOCKET_INFORMATION, *LPSOCKET_INFORMATION;
