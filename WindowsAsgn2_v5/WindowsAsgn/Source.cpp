ddad2d5e1b2ce7356dce2ffa7078dd155f5fbec8
unknown <tmanthegamer@gmail.com>
2016-02-17 16:50:37 -0800

    pants.

--- WindowsAsgn2_v5/WindowsAsgn/Source.cpp
+++ WindowsAsgn2_v5/WindowsAsgn/Source.cpp
@@ -1238,7 +1238,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message,
 			packet_size = 60000;
 			break;
 		case ID_PACKETSIZE_CUSTOM:
-			DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_ABOUT), hwnd, AboutDlgProc);
 			break;
 		case ID_FREQUENCY_1:
 			frequency = 1;
@@ -1336,22 +1335,14 @@ void GetTextFromHost() {
 }
 
 void GetTextFromPort() {
+	int temp = protocol;
+	
 	int len = SendMessage(inputPort, WM_GETTEXTLENGTH, 0, 0);
 	TCHAR* buffer = new char[len + 1];
 	SendMessage(inputPort, WM_GETTEXT, (WPARAM)len + 1, (LPARAM)buffer);
 	sscanf_s(buffer, "%zu", &port);
-}
 
-void SetOutputText(TCHAR* text, int len) {
-	//SendMessage(output, WM_SETTEXT, (WPARAM)len, (LPARAM)text);
-}
-
-void AppendOutputText(TCHAR* text) {
-	DWORD l, r;
-
-	//SendMessage(output, EM_GETSEL, (WPARAM)&l, (LPARAM)&r);
-	//SendMessage(output, EM_SETSEL, -1, -1);
-	//SendMessage(output, EM_REPLACESEL, TRUE, (LPARAM)text);
+	protocol = temp;
 }
 
 void updateStatistic(HWND hList, float savg, float ravg, int sent, int recv, double data, int time) {
